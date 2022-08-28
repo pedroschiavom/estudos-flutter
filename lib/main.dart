f6db5245c27f057f9677e076bf68bc9bcbbb72bc
Pedro Schiavom <pedro.schiavon@mcs-it.com.br>
2022-08-28 17:03:11 -0300

    adding map method to create the answers for each question

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,27 @@ class _PerguntaAppState extends State<PerguntaApp> {
   @override
   Widget build(BuildContext context) {
     final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?'
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      },
+      {
+        'texto': 'Qual é o seu instrutor favorito?',
+        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      },
     ];
+
+    List<String> respostas =
+        perguntas[_perguntaSelecionada].cast()['respostas'];
+    List<Widget> widgets;
+
+    //for (String textoResp in respostas) {
+    //widgets.add(Resposta(textoResp, _responder));
+    //}
 
     return MaterialApp(
       home: Scaffold(
@@ -29,10 +47,8 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas.map((t) => Resposta(t, _responder)).toList(),
           ],
         ),
       ),
--- lib/questao.dart
+++ lib/questao.dart
@@ -1,9 +1,6 @@
-
 import 'package:flutter/material.dart';
 
 class Questao extends StatelessWidget {
- 
-
   final String texto;
 
   Questao(this.texto);
