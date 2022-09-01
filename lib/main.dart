86c7e065b6a78138306287a6c4b1ebc64502138e
pedroschiavomext <pedro.schiavon@mcs-it.com.br>
2022-09-01 17:43:03 -0300

    created system of points in questions and button to restart the questions

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,48 @@ main() {
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
   final _perguntas = const [
     {
       'texto': 'Qual é a sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      'respostas': [
+        {'texto':'Preto','pontuacao': 10}, 
+        {'texto':'Vermelho','pontuacao': 5},
+        {'texto':'Verde','pontuacao': 3},
+        {'texto':'Branco','pontuacao': 1}],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
-      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      'respostas': [
+        {'texto':'Coelho','pontuacao': 10},
+        {'texto':'Cobra','pontuacao': 5},
+        {'texto':'Elefante','pontuacao': 3},
+        {'texto':'Leão','pontuacao': 1},],
     },
     {
       'texto': 'Qual é o seu instrutor favorito?',
-      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      'respostas': [
+        {'texto':'Maria','pontuacao': 10},
+        {'texto':'João','pontuacao': 5},
+        {'texto':'Leo','pontuacao': 3},
+        {'texto':'Pedro','pontuacao': 1},],
     },
   ];
 
-  void _responder() {
+  void _responder(int pontuacao) {
     if (temPerguntaSelecionada) {
       setState(() {
         _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
       });
     }
+  }
+
+  void _reiniciarQuestionario(){
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
   }
 
   bool get temPerguntaSelecionada {
@@ -55,7 +76,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
             perguntaSelecionada: _perguntaSelecionada, 
             quandoResponder: _responder,
             )
-            : Resultado(),
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
--- lib/questionario.dart
+++ lib/questionario.dart
@@ -5,14 +5,12 @@ import 'package:projeto_perguntas/resposta.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() quandoResponder;
+  final void Function(int) quandoResponder;
 
-  Questionario({
-    required this.perguntas,
-    required this.perguntaSelecionada,
-    required this.quandoResponder
-  });
-
+  Questionario(
+      {required this.perguntas,
+      required this.perguntaSelecionada,
+      required this.quandoResponder});
 
   bool get temPerguntaSelecionada {
     return perguntaSelecionada < perguntas.length;
@@ -20,14 +18,17 @@ class Questionario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
         ? perguntas[perguntaSelecionada].cast()['respostas']
         : [];
 
     return Column(
       children: <Widget>[
         Questao(perguntas[perguntaSelecionada]['texto'].toString()),
-        ...respostas.map((t) => Resposta(t, quandoResponder)).toList(),
+        ...respostas.map((resp) {
+          return Resposta(resp['texto'].toString(),
+              () => quandoResponder(int.parse(resp['pontuacao'].toString())));
+        }).toList(),
       ],
     );
   }
